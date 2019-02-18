8add4eb33f469ece5995d689b4715fa1fddf87fe
Ben Jen <benjendrzejak@quickenloans.com>
2019-02-18 11:13:37 -0500

    Front end fixed, backend POST WIP

---
+++ APILab.Inventory/Controllers/InventoriesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APILab.Inventory.DAL;
+using System.Web.Http.Cors;
+
+namespace APILab.Inventory.Controllers
+{
+    [EnableCors(origins: "http://localhost:32534", headers: "*", methods: "*")]
+    public class InventoriesController : ApiController
+    {
+        private InventoryDbContext db = new InventoryDbContext();
+
+        // GET: api/Inventories
+        public IQueryable<Models.Inventory> GetInventories()
+        {
+            return db.Inventories;
+        }
+
+        // GET: api/Inventories/5
+        [ResponseType(typeof(Models.Inventory))]
+        public async Task<IHttpActionResult> GetInventory(int id)
+        {
+            Models.Inventory inventory = await db.Inventories.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inventory);
+        }
+
+        // POST: api/Inventories
+        [ResponseType(typeof(Models.Inventory))]
+        public async Task<IHttpActionResult> PostInventory(Models.Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Inventories.Add(inventory);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = inventory.Id }, inventory);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
